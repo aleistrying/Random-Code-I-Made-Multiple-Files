3bc2916a84279696abb30c5e868aa3903f2d370f
Alejandro P <alejandroparparcen@yahoo.com>
2019-04-13 22:41:15 -0500

    Add files via upload

---
+++ WindowsFormsApp for testing/WindowsFormsApp for testing/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace WindowsFormsApp_for_testing
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            using (var fbd = new FolderBrowserDialog())
+            {
+                DialogResult result = fbd.ShowDialog();
+
+                if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath))
+                {
+                    string[] files = Directory.GetFiles(fbd.SelectedPath);
+
+                    System.Windows.Forms.MessageBox.Show("Files found: " + files.Length.ToString(), "Message");
+                }
+            }
+        }
+    }
+}
---
+++ WriteNumbers/WriteNumbers/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WriteNumbers
+{
+    class Program
+    {
+        static int x = 700;
+        static void Main(string[] args)
+        {
+            while (x < 1000)
+            {
+                Console.WriteLine(x);
+                x++;
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
